d509db4bebcc41f0f4622f5f51671bb9bbd3c4e7
ARVIND_VERMA <arvindverma630635@gmail.com>
2024-12-02 23:34:06 +0530

    new ui

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,5 +5,5 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
 <!-- Google Fonts Roboto -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
-<link href="{{asset('css/page.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('css/style.css')}}?v={{time()}}"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,87 +1,59 @@
-<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+    <!-- Container wrapper -->
+    <div class="container">
+      <!-- Navbar brand -->
+      <a class="navbar-brand me-2" href="https://mdbgo.com/">
+        <img
+          src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+          height="16"
+          alt="MDB Logo"
+          loading="lazy"
+          style="margin-top: -1px;"
+        />
+      </a>
 
-<div class="wrapper d-flex align-items-stretch">
-    <nav id="sidebar">
-        <div class="p-4 pt-5" style="position: fixed; width: 300px; height: 100vh; overflow:scroll;
-    overflow-x:hidden;">
-            <a href="#" class="img logo rounded-circle mb-5" style="background-image: url(images/icon.png);"></a>
-            <ul class="list-unstyled components mb-5">
-                <li class="active">
-                    <a href="#homeSubmenu">Home</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false"
-                        class="dropdown-toggle">Laravel</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
+      <!-- Toggle button -->
+      <button
+        data-mdb-collapse-init
+        class="navbar-toggler"
+        type="button"
+        data-mdb-target="#navbarButtonsExample"
+        aria-controls="navbarButtonsExample"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
 
-            </ul>
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarButtonsExample">
+        <!-- Left links -->
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Dashboard</a>
+          </li>
+        </ul>
+        <!-- Left links -->
 
-            <div class="footer">
-                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;
-                    <script>
-                        document.write(new Date().getFullYear());
-                    </script> All rights reserved |<i class="icon-heart"
-                        aria-hidden="true"></i> by <a href="https://codebuilder.in" target="_blank">Codebuilder.in</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                </p>
-            </div>
-
+        <div class="d-flex align-items-center">
+          <button data-mdb-ripple-init type="button" class="btn btn-link px-3 me-2">
+            Login
+          </button>
+          <button data-mdb-ripple-init type="button" class="btn btn-primary me-3">
+            Sign up for free
+          </button>
+          <a
+            data-mdb-ripple-init
+            class="btn btn-dark px-3"
+            href="https://github.com/mdbootstrap/mdb-ui-kit"
+            role="button"
+            ><i class="fab fa-github"></i
+          ></a>
         </div>
-    </nav>
-
-    <!-- Page Content  -->
-    <div id="content" class="p-4 p-md-5">
-
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <!-- Sidebar Toggle Button -->
-                <button type="button" id="sidebarCollapse" class="btn btn-primary">
-                    <i class="fa fa-bars"></i>
-                    <span class="visually-hidden">Toggle Menu</span>
-                </button>
-
-                <!-- Navbar Toggle Button for Mobile -->
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </button>
-
-                <!-- Navbar Links -->
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-                        <div class="input-group" style="flex-wrap: nowrap">
-                            <div class="form-outline" data-mdb-input-init>
-                                <input id="search-focus" type="search" id="form1" class="form-control" />
-                                <label class="form-label" for="form1">Search</label>
-                            </div>
-                            <button type="button" class="btn btn-primary" data-mdb-ripple-init>
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </div>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+      </div>
+      <!-- Collapsible wrapper -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
