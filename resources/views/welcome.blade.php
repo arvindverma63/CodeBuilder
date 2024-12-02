9a141e57a372f07e68b7d595dc217027a197c2ee
ARVIND_VERMA <arvindverma630635@gmail.com>
2024-12-03 00:01:09 +0530

    second day

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     @include('partials.head')
-    <link rel="stylesheet" href="{{asset('css/page.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/page.css') }}">
 </head>
+
 <body>
 
-@include('partials.nav')
+    @include('partials.nav')
+
+    <div class="side-container card">
+        <div class="sidebar">
+            <ul class="sidebar-list">
+                <li class="active">Dashboard</li>
+                <li>Laravel</li>
+                <li>PHP</li>
+                <li>Wordpress</li>
+                <li>Android</li>
+                <li>Java</li>
+                <li>React</li>
+            </ul>
+        </div>
+    </div>
 
 
-
-
-@include('partials.footer')
-@include('partials.js')
+    @include('partials.footer')
+    @include('partials.js')
 
 </body>
+
 </html>
